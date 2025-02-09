70deb2f618edde6bdaa880c6472c30b84537491b
PhaneronGlobal <kabir.chhabra@hotmail.com>
2025-02-09 21:48:23 +0000

    pls

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,13 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+// If you’re using Wouter for routing, import { Router } from "wouter";
 import App from "./App";
-import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    {/* <Router>  // Uncomment if you’re using Wouter routing
+        <App />
+      </Router> */}
+    <App />
+  </React.StrictMode>
+);
